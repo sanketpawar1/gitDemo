5e89d0d0d1fdd586cb661edb0201504af9d2bc94
sanket pawar <sanket pawar@DESKTOP-D2RPL58>
2022-12-14 14:12:27 +0530

    first commit...

---
+++ src/demo1/If.java
@@ -0,0 +1,20 @@
+package demo1;
+
+public class If {
+
+	public static void main(String[] args) 
+	{
+float temp=37.5f;
+		
+		if (temp>37.5)
+		{
+			System.out.println("Turn on AC");
+		}
+		
+		else
+		{
+			System.out.println("Turn off AC");
+		}
+	}
+
+}
